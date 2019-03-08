d964822cf6afcd2994f17ce6d06c74f8de674a36
Landon Wright <Landon.wright91@gmail.com>
2019-03-08 13:26:28 -0700

    move to plot recipes

--- src/helper.jl
+++ src/helper.jl
@@ -9,3 +9,31 @@ function getways(data::OSMData, key::String, value::String)
     isfeature(w::Int) = get(tags(w), key, "") == value
     wayids = filter(isfeature, collect(keys(data.tags)))
 end
+
+function wayidtopoints(osmdata::OSMData, wayid)
+    lat = Array{Float64, 1}()
+    lon = Array{Float64, 1}()
+    nodeids = get(osmdata.ways, wayid, [wayid])
+    for node in nodeids
+        if haskey(osmdata.nodes, node)
+            nodelat, nodelon = osmdata.nodes[node]
+            push!(lat, nodelat)
+            push!(lon, nodelon)
+        end
+    end
+    lat, lon
+end
+
+function collectpoints(osmdata::OSMData, key::String, value::String)
+    wayids = findways(osmdata, key, value)
+    lats = Array{Float64, 1}()
+    lons = Array{Float64, 1}()
+    for way in wayids
+        lat, lon = wayidtopoints(osmdata, way)
+        for (t,n) in zip(lat, lon)
+            push!(lats, t)
+            push!(lons, n)
+        end
+    end
+    lats, lons
+end
--- src/viewer.jl
+++ src/viewer.jl
@@ -6,81 +6,88 @@ struct OSMPaths
     pathtypes::Array{String, 1}
 end
 
-function osmways(osmdata::OSMData, access::Dict{String,Symbol}=ACCESS["motorcar"])
-    # find all the paths that the given access type can use
+# function osmways(osmdata::OSMData, access::Dict{String,Symbol}=ACCESS["motorcar"])
+#     # find all the paths that the given access type can use
+#     tags(w::Int) = get(osmdata.tags, w, Dict{String,String}())
+#     lookup(tags::Dict{String,String}, k::String) = get(tags, k, "")
+#     hasaccess(w::Int) = get(access, lookup(tags(w),"highway"), :no) != :no
+#     ishighway(w::Int) = haskey(tags(w), "highway")
+#     wayids = filter(hasaccess, filter(ishighway, collect(keys(osmdata.ways))))
+
+#     pathtypes = collect(keys(access))
+#     noaccess = get.(Ref(access), collect(keys(access)), :no).==:no
+#     approvedpaths = pathtypes[.!noaccess]
+
+#     numnodes = length(keys(osmdata.nodes))
+#     # store each type of path in an array that we can pass to the plot command
+#     latPaths = Dict{String, Array{Array{Float64, 1},1}}()
+#     lonPaths = Dict{String, Array{Array{Float64, 1},1}}()
+#     for pathtype in approvedpaths
+#         latPaths[pathtype] = Array{Float64, 1}()
+#         lonPaths[pathtype] = Array{Float64, 1}()
+#     end
+#     for wayid in wayids
+#         #get way points
+#         lat=[]
+#         lon=[]
+#         way = osmdata.ways[wayid]
+#         for ii in 1:length(osmdata.ways[wayid])
+#             if haskey(osmdata.nodes, way[ii])
+#                 (nodelat, nodelon) = osmdata.nodes[way[ii]]
+#                 push!(lat, nodelat)
+#                 push!(lon, nodelon)
+#             else
+#                 println("couldn't find node $(way[ii])")
+#             end
+#         end
+#         #determine path type
+#         waytype = lookup(tags(wayid), "highway")
+#         #ignore things if it is a parking lot
+#         #add points to array
+#         if haskey(latPaths, waytype)
+#             push!(latPaths[waytype], lat)
+#             push!(lonPaths[waytype], lon)
+#         else
+#             error("Couldn't find key $waytype in the paths")
+#         end
+#     end
+#     return OSMPaths(latPaths, lonPaths, approvedpaths)
+# end
+
+# function plotmap(waypoints::OSMPaths)
+#     latpaths = waypoints.latpaths
+#     lonpaths = waypoints.lonpaths
+#     p = newplot()
+#     for pathtype in waypoints.pathtypes
+#         nlines = size(lonpaths[pathtype], 1)
+#         if nlines == 0
+#             continue
+#         end
+
+#         # println(lonPaths[pathtype],"\n\n\n",size(latPaths[pathtype]), latPaths[pathtype])
+#         # println("ahhh")
+#         Plots.plot!(lonpaths[pathtype][1], latpaths[pathtype][1], label=pathtype, color=MAP_COLORS[pathtype])
+#         Plots.plot!(lonpaths[pathtype][2:end], latpaths[pathtype][2:end], label="", color=MAP_COLORS[pathtype])
+#     end
+#     Plots.gui()
+#     return p
+# end
+
+# function plotmap(osmdata::OSMData, access::Dict{String,Symbol}=ACCESS["motorcar"])
+#     ways = osmways(osmdata, access)
+#     plotmap(ways)
+# end
+
+# function newplot()
+#     return Plots.plot(size=[1200,1000])
+# end
+
+function findways(osmdata::OSMData, key::String, value::String)
     tags(w::Int) = get(osmdata.tags, w, Dict{String,String}())
-    lookup(tags::Dict{String,String}, k::String) = get(tags, k, "")
-    hasaccess(w::Int) = get(access, lookup(tags(w),"highway"), :no) != :no
-    ishighway(w::Int) = haskey(tags(w), "highway")
-    wayids = filter(hasaccess, filter(ishighway, collect(keys(osmdata.ways))))
-
-    pathtypes = collect(keys(access))
-    noaccess = get.(Ref(access), collect(keys(access)), :no).==:no
-    approvedpaths = pathtypes[.!noaccess]
-
-    numnodes = length(keys(osmdata.nodes))
-    # store each type of path in an array that we can pass to the plot command
-    latPaths = Dict{String, Array{Array{Float64, 1},1}}()
-    lonPaths = Dict{String, Array{Array{Float64, 1},1}}()
-    for pathtype in approvedpaths
-        latPaths[pathtype] = Array{Float64, 1}()
-        lonPaths[pathtype] = Array{Float64, 1}()
-    end
-    for wayid in wayids
-        #get way points
-        lat=[]
-        lon=[]
-        way = osmdata.ways[wayid]
-        for ii in 1:length(osmdata.ways[wayid])
-            if haskey(osmdata.nodes, way[ii])
-                (nodelat, nodelon) = osmdata.nodes[way[ii]]
-                push!(lat, nodelat)
-                push!(lon, nodelon)
-            else
-                println("couldn't find node $(way[ii])")
-            end
-        end
-        #determine path type
-        waytype = lookup(tags(wayid), "highway")
-        #ignore things if it is a parking lot
-        #add points to array
-        if haskey(latPaths, waytype)
-            push!(latPaths[waytype], lat)
-            push!(lonPaths[waytype], lon)
-        else
-            error("Couldn't find key $waytype in the paths")
-        end
-    end
-    return OSMPaths(latPaths, lonPaths, approvedpaths)
+    isfeature(w::Int) = get(tags(w), key, "") == value
+    wayids = filter(isfeature, collect(keys(osmdata.tags)))
 end
 
-function plotmap(waypoints::OSMPaths)
-    latpaths = waypoints.latpaths
-    lonpaths = waypoints.lonpaths
-    p = newplot()
-    for pathtype in waypoints.pathtypes
-        nlines = size(lonpaths[pathtype], 1)
-        if nlines == 0
-            continue
-        end
-
-        # println(lonPaths[pathtype],"\n\n\n",size(latPaths[pathtype]), latPaths[pathtype])
-        # println("ahhh")
-        Plots.plot!(lonpaths[pathtype][1], latpaths[pathtype][1], label=pathtype, color=MAP_COLORS[pathtype])
-        Plots.plot!(lonpaths[pathtype][2:end], latpaths[pathtype][2:end], label="", color=MAP_COLORS[pathtype])
-    end
-    Plots.gui()
-    return p
-end
-
-function plotmap(osmdata::OSMData, access::Dict{String,Symbol}=ACCESS["motorcar"])
-    ways = osmways(osmdata, access)
-    plotmap(ways)
-end
-
-function newplot()
-    return Plots.plot(size=[1200,1000])
-end
 
 @recipe function f(osmdata::OSMData, access::Dict{String, Symbol}=ACCESS["motorcar"])
     hasaccess(ty::String) = !(access[ty] == :no)
@@ -90,103 +97,119 @@ end
         tags(w::Int) = get(osmdata.tags, w, Dict{String,String}())
         isfeature(w::Int) = get(tags(w), "highway", "") == pathtype
         wayids = filter(isfeature, collect(keys(osmdata.tags)))
-        
         numnodes = length(keys(osmdata.nodes))
-
-        lats = Array{Array{Float64, 1},1}()
-        lons = Array{Array{Float64, 1},1}()
-        # baseplot = plotfeature(osmdata, "highway", pathtype, baseplot, color=MAP_COLORS[pathtype]; kwargs...)
         for wayid in wayids
-            lat = Array{Float64,1}()
-            lon = Array{Float64,1}()
-            # println("looking for $wayid")
-            way = get(osmdata.ways, wayid, Array{Float64,1}(undef,1))
-            # way = osmdata.ways[wayid]
-            for ii in 1:length(way)
-                if haskey(osmdata.nodes, way[ii])
-                    (nodelat, nodelon) = osmdata.nodes[way[ii]]
-                    push!(lat, nodelat)
-                    push!(lon, nodelon)
-                else
-                    # println("couldn't find node $(way[ii])")
-                end
+
+            lat, lon = wayidtopoints(osmdata, wayid)
+            @series begin
+                color := MAP_COLORS[pathtype]
+                label := ""
+                lon, lat
             end
-            push!(lats, lat)
-            push!(lons, lon)
-        end
-        @series begin
-            color := MAP_COLORS[pathtype]
-            label := ""
-            lons, lats
         end
     end
 
 end
 
-function plotosm(osmdata::OSMData; access::Dict{String, Symbol}=ACCESS["motorcar"], baseplot::Plots.Plot=newplot(), kwargs...)
-    kwargs = collect(kwargs)
-    hasaccess(ty::String) = !(access[ty] == :no)
-    hasaccess(pair::Pair{String, Symbol}) = pair.second != :no
-    accessable = filter(hasaccess, access)
-    for pathtype in collect(keys(accessable))
-        baseplot = plotfeature(osmdata, "highway", pathtype, baseplot, color=MAP_COLORS[pathtype]; kwargs...)
-    end
-    Plots.gui()
-    return baseplot
-end
+@userplot drawfeature
 
-function plotfeature(osmdata::OSMData, key::String, value::String, baseplot::Plots.Plot=newplot(); kwargs...)
-    tags(w::Int) = get(osmdata.tags, w, Dict{String,String}())
-    isfeature(w::Int) = get(tags(w), key, "") == value
-    wayids = filter(isfeature, collect(keys(osmdata.tags)))
+@recipe function f(h::drawfeature)
+    count = 0
+    if length(h.args) != 3
+        error("incorrect number of inputs")
+    end
+    osmdata, key, value = h.args
+    wayids = findways(osmdata, key, value)
 
     numnodes = length(keys(osmdata.nodes))
-
-    lats = Array{Array{Float64, 1},1}()
-    lons = Array{Array{Float64, 1},1}()
+    cid = rand(1:15)
+    # color --> rand(1:15)
     for wayid in wayids
-        lat = Array{Float64,1}()
-        lon = Array{Float64,1}()
-        # println("looking for $wayid")
-        way = get(osmdata.ways, wayid, Array{Float64,1}(undef,1))
-        # way = osmdata.ways[wayid]
-        for ii in 1:length(way)
-            if haskey(osmdata.nodes, way[ii])
-                (nodelat, nodelon) = osmdata.nodes[way[ii]]
-                push!(lat, nodelat)
-                push!(lon, nodelon)
+        lat, lon = wayidtopoints(osmdata, wayid)
+        @series begin
+            if length(lon) > 2
+                seriestype --> :shape
             else
-                # println("couldn't find node $(way[ii])")
+                seriestype --> :scatter
             end
+            if count < 1
+                label --> "$key: $value"
+            else
+                label := ""
+            end
+            color --> cid
+            lon, lat
         end
-        push!(lats, lat)
-        push!(lons, lon)
+        count += 1
     end
-    p = Plots.plot!(baseplot, lons, lats; kwargs...)
+    # p = Plots.plot!(baseplot, lons, lats; kwargs...)
 end
 
-function plotnodesequence(osmdata::OSMData, nodes::Array{Int64,1}, baseplot::Plots.Plot; kwargs...)
-    lats = Array{Float64, 1}()
-    lons = Array{Float64, 1}()
-    # numnodes = length(osmdata.nodes.id)
-    # nodeid = Dict(zip(osmdata.nodes.id, 1:numnodes))
-    for node in nodes
-        (nodelat, nodelon) = osmdata.nodes[node]
-        push!(lats, nodelat)
-        push!(lons, nodelon)
-        # push!(lats, osmdata.nodes.lat[nodeid[node]])
-        # push!(lons, osmdata.nodes.lon[nodeid[node]])
-    end
-    p = Plots.plot!(baseplot, lons, lats; kwargs...)
-end
+# function plotosm(osmdata::OSMData; access::Dict{String, Symbol}=ACCESS["motorcar"], baseplot::Plots.Plot=newplot(), kwargs...)
+#     kwargs = collect(kwargs)
+#     hasaccess(ty::String) = !(access[ty] == :no)
+#     hasaccess(pair::Pair{String, Symbol}) = pair.second != :no
+#     accessable = filter(hasaccess, access)
+#     for pathtype in collect(keys(accessable))
+#         baseplot = plotfeature(osmdata, "highway", pathtype, baseplot, color=MAP_COLORS[pathtype]; kwargs...)
+#     end
+#     Plots.gui()
+#     return baseplot
+# end
 
-function plotedgearray(network::OSMNetwork, edges::Array{LightGraphs.SimpleGraphs.SimpleEdge, 1}, baseplot::Plots.Plot=newplot(); kwargs...)
-    lightgraphpath = LightGraphs.src.(edges)
-    push!(lightgraphpath, LightGraphs.dst(edges[end]))
-    p = plotLGnodesequence(network, lightgraphpath, baseplot; kwargs...) 
-end
+# function plotfeature(osmdata::OSMData, key::String, value::String, baseplot::Plots.Plot=newplot(); kwargs...)
+#     tags(w::Int) = get(osmdata.tags, w, Dict{String,String}())
+#     isfeature(w::Int) = get(tags(w), key, "") == value
+#     wayids = filter(isfeature, collect(keys(osmdata.tags)))
 
-function plotLGnodesequence(network::OSMNetwork, nodes::Array{Int64,1}, baseplot::Plots.Plot=newplot(); kwargs...)
-    osmpath = [network.nodeid[lightgraphid] for lightgraphid in nodes]
-    p = plotnodesequence(network.data, osmpath, baseplot; kwargs...)
-end
+#     numnodes = length(keys(osmdata.nodes))
+
+#     lats = Array{Array{Float64, 1},1}()
+#     lons = Array{Array{Float64, 1},1}()
+#     for wayid in wayids
+#         lat = Array{Float64,1}()
+#         lon = Array{Float64,1}()
+#         # println("looking for $wayid")
+#         # way = get(osmdata.ways, wayid, Array{Float64,1}(undef,1))
+#         way = get(osmdata.ways, wayid, get(osmdata.nodes, wayid, Array{Float64, 1}(undef,1)))
+#         # way = osmdata.ways[wayid]
+#         for ii in 1:length(way)
+#             if haskey(osmdata.nodes, way[ii])
+#                 (nodelat, nodelon) = osmdata.nodes[way[ii]]
+#                 push!(lat, nodelat)
+#                 push!(lon, nodelon)
+#             else
+#                 # println("couldn't find node $(way[ii])")
+#             end
+#         end
+#         push!(lats, lat)
+#         push!(lons, lon)
+#     end
+#     p = Plots.plot!(baseplot, lons, lats; kwargs...)
+# end
+
+# function plotnodesequence(osmdata::OSMData, nodes::Array{Int64,1}, baseplot::Plots.Plot; kwargs...)
+#     lats = Array{Float64, 1}()
+#     lons = Array{Float64, 1}()
+#     # numnodes = length(osmdata.nodes.id)
+#     # nodeid = Dict(zip(osmdata.nodes.id, 1:numnodes))
+#     for node in nodes
+#         (nodelat, nodelon) = osmdata.nodes[node]
+#         push!(lats, nodelat)
+#         push!(lons, nodelon)
+#         # push!(lats, osmdata.nodes.lat[nodeid[node]])
+#         # push!(lons, osmdata.nodes.lon[nodeid[node]])
+#     end
+#     p = Plots.plot!(baseplot, lons, lats; kwargs...)
+# end
+
+# function plotedgearray(network::OSMNetwork, edges::Array{LightGraphs.SimpleGraphs.SimpleEdge, 1}, baseplot::Plots.Plot=newplot(); kwargs...)
+#     lightgraphpath = LightGraphs.src.(edges)
+#     push!(lightgraphpath, LightGraphs.dst(edges[end]))
+#     p = plotLGnodesequence(network, lightgraphpath, baseplot; kwargs...) 
+# end
+
+# function plotLGnodesequence(network::OSMNetwork, nodes::Array{Int64,1}, baseplot::Plots.Plot=newplot(); kwargs...)
+#     osmpath = [network.nodeid[lightgraphid] for lightgraphid in nodes]
+#     p = plotnodesequence(network.data, osmpath, baseplot; kwargs...)
+# end
